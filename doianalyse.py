2fe671a09d0dc57cb5c7817fb643100258f240d7
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-02-26 14:36:28 +0700

    Update doianalyse.py

--- doianalyse.py
+++ doianalyse.py
@@ -61,7 +61,7 @@ selected_product = st.selectbox("Select Product ID", product_ids)
 filtered_df = analisa_df[analisa_df['product_id'] == selected_product]
 
 fig, ax = plt.subplots(figsize=(10, 5))
-ax.barh(['Landed DOI New', 'Landed DOI OLD'], [analisa_df['Landed DOI New'].mean(), analisa_df['Landed DOI Old'].mean()], color=['g', 'r'])
+ax.barh(['Landed DOI New', 'Landed DOI OLD'], [analisa_df['Landed DOI New'].mean(), analisa_df['Landed DOI OLD'].mean()], color=['g', 'r'])
 ax.set_xlabel('Landed DOI')
 ax.set_title('Comparison of Landed DOI New vs Old')
 ax.grid()
