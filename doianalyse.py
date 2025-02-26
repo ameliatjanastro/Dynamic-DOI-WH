fb3cf3b54708aa6b2585f9f096742d54893b70bb
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-02-26 20:55:36 +0700

    Update doianalyse.py

--- doianalyse.py
+++ doianalyse.py
@@ -40,9 +40,7 @@ analisa_df[['Landed DOI New', 'Landed DOI OLD']] = analisa_df[['Landed DOI New',
 analisa_df['RL Qty Actual'] = analisa_df['RL Qty Actual'].fillna(0)
 analisa_df['RL Qty NEW after MIN QTY WH'] = analisa_df['RL Qty NEW after MIN QTY WH'].fillna(0)
 
-filtered_df1 = analisa_df[
-    (analisa_df['RL Qty Actual'] == 0) & (analisa_df['RL Qty NEW after MIN QTY WH'] != 0)
-][['Product ID', 'RL Qty Actual', 'RL Qty NEW after MIN QTY WH']]
+filtered_df1 = analisa_df[(analisa_df['RL Qty Actual'] == 0) & (analisa_df['RL Qty NEW after MIN QTY WH'] != 0)][['Product ID', 'RL Qty Actual', 'RL Qty NEW after MIN QTY WH']]
 
 # Calculate summary statistics
 total_products = filtered_df1['Product ID'].nunique()  # Count unique products
