7894eb33b07da633554a506929a19fdd29975522
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-02-26 17:57:56 +0700

    Update doianalyse.py

--- doianalyse.py
+++ doianalyse.py
@@ -47,8 +47,8 @@ sum_rl_qty_actual = analisa_df['RL Qty Actual'].sum()
 sum_rl_qty_new = analisa_df['RL Qty NEW after MIN QTY WH'].sum()
 
 # Exclude Landed DOI values greater than 21 before calculating the average
-filtered_doi_df = analisa_df[analisa_df['Landed DOI New'] <= 21]
-filtered_doi_old_df = analisa_df[analisa_df['Landed DOI OLD'] <= 21]
+#filtered_doi_df = analisa_df[analisa_df['Landed DOI New'] <= 21]
+#filtered_doi_old_df = analisa_df[analisa_df['Landed DOI OLD'] <= 21]
 
 avg_landed_doi_new = filtered_doi_df['Landed DOI New'].mean()
 avg_landed_doi_old = filtered_doi_old_df['Landed DOI OLD'].mean()
