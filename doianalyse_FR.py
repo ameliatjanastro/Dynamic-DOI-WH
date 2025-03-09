c2ee7d2f9c9bfa05cd8bf24f5023c5cc67952c5d
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-03-09 17:42:24 +0700

    Update doianalyse_FR.py

--- doianalyse_FR.py
+++ doianalyse_FR.py
@@ -260,7 +260,7 @@ with col2:
 if selected_category != "All":
     filtered_df = filtered_df[filtered_df['l1_category_name'] == selected_category]
 
-filtered_df = filtered_df[filtered_df['Landed DOI OLD'] != 0]
+filtered_df = filtered_df[filtered_df['Landed DOI OLD'] != 0) & (~filtered_df['product_id'].isin(excluded_df['product_id']))]
 
 # Calculate averages
 avg_landed_doi_new = filtered_df['Landed DOI New'].mean()
